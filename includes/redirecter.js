a433dca3d9ce86bd71e2beb8a271ce10e62230d7
Daniel Aleksandersen <code@daniel.priv.no>
2012-02-17 00:41:43 +0100

    Changed shortcut to Control–h to allign it across all platforms. Mac mislabels the ctrl key as meta. Other platforms all behave predictable. It’s window.navigator and not navigator.

--- includes/redirecter.js
+++ includes/redirecter.js
@@ -3,29 +3,18 @@
     window.location = redirect.data;
   }, false);
   document.addEventListener('keypress', function(keys) {
-    if (~navigator.platform.indexOf('Mac'))
+    if (~window.navigator.platform.indexOf('Mac'))
     {
-      if (keys.keyCode == '8' && keys.metaKey && keys.altKey) // control–alt–h
+      if (keys.keyCode == '8' && keys.metaKey) // control–h, ctrl is mislabeled as meta on Mac
       {
         opera.extension.postMessage('keyboardshortcut');
         keys.preventDefault();
       }
     }
-    if (~navigator.platform.indexOf('Win'))
+    else if (keys.keyCode == '104' && keys.ctrlKey) // control–h
     {
-      if (keys.keyCode == '104' && keys.ctrlKey && keys.altKey) // control–alt–h
-      {
-        opera.extension.postMessage('keyboardshortcut');
-        keys.preventDefault();
-      }
-    }
-    if (~navigator.platform.indexOf('Linux'))
-    {
-      if (keys.keyCode == '104' && keys.ctrlKey) // control–h
-      {
-        opera.extension.postMessage('keyboardshortcut');
-        keys.preventDefault();
-      }
+      opera.extension.postMessage('keyboardshortcut');
+      keys.preventDefault();
     }
   }, false);
 }());
