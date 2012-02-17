e9f0fd0d5ad717dcd127045120be39eb336e0458
Daniel Aleksandersen <code@daniel.priv.no>
2012-02-17 04:06:25 +0100

    Simplified platform specific keyboard shortcut handling.

--- includes/redirecter.js
+++ includes/redirecter.js
@@ -3,15 +3,7 @@
     window.location = redirect.data;
   }, false);
   document.addEventListener('keypress', function(keys) {
-    if (~window.navigator.platform.indexOf('Mac'))
-    {
-      if (keys.keyCode == '8' && keys.metaKey) // control–h, ctrl is mislabeled as meta on Mac
-      {
-        opera.extension.postMessage('keyboardshortcut');
-        keys.preventDefault();
-      }
-    }
-    else if (keys.keyCode == '104' && keys.ctrlKey) // control–h
+    if ((keys.keyCode == '104' && keys.ctrlKey) || (keys.keyCode == '8' && keys.metaKey)) // control–h, ctrl is mislabeled as meta on Mac
     {
       opera.extension.postMessage('keyboardshortcut');
       keys.preventDefault();
