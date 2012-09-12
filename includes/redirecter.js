f32a43f530333afe0cd251ce8f08e836c7b541a6
Daniel Aleksandersen <code@daniel.priv.no>
2012-09-13 02:47:02 +0300

    UA detection is a bad practice.
    Turned out we are releasing it as Opera 12.10 instead of 12.50

--- includes/redirecter.js
+++ includes/redirecter.js
@@ -4,7 +4,7 @@
   document.addEventListener('keydown', function (event) {
     var ctrlKey = event.ctrlKey;
     // Old versions of Opera on Mac swapped the Control and Meta keys
-    if (((window.navigator.platform).indexOf('Mac') !== -1) && (window.opera.version() < 12.50)) {
+    if (((window.navigator.platform).indexOf('Mac') !== -1) && (window.opera.version() < 12.10)) {
       ctrlKey = event.metaKey;
     }
     if (event.keyCode === 72 && ctrlKey) { // Control–H
