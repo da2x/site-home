c3a09a59e8e022fa958b46dd1c573834d9b3f15f
Daniel Aleksandersen <code@daniel.priv.no>
2014-01-29 17:31:46 +0100

    Do not update tab if tab is a home page
    Resolves Opera Extension Catalog report #94695.

--- src/background.js
+++ src/background.js
@@ -1,7 +1,9 @@
 chrome.browserAction.onClicked.addListener( function( activeTab ) {
   var activeUrl = activeTab.url.split( '/' ),
-    protocol = activeUrl[0],
-    hostname = activeUrl[2],
-    homeAddr = protocol + '//' + hostname
-  chrome.tabs.update( { url: homeAddr } )
+    protocol  = activeUrl[0],
+    hostname  = activeUrl[2],
+    homeAddr  = protocol + '//' + hostname,
+    isNotHome = activeUrl[3];
+    if ( !isNotHome )
+      chrome.tabs.update( { url: homeAddr } );
 })
